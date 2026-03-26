40c835482c7875118f3c1191d2fe4634ee801f0e
himanshijoshi987093-ux <himanshijoshi987093@gmail.com>
2026-03-26 20:53:30 +0530

    This is my first bank project in c lunguage

---
+++ myfuncations/bank_project.c/create_account.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+int createaccount()
+{
+
+}
+int withdrawamount()
+{
+
+}
+
+{
+ printf("1. Create account:");
+ Printf("2.Withdraw amount:");
+ printf("3.Deposit Amount:");
+ printf("4.Check Blance:");
+ peintf("5.Exit");
+
+
+
+
+
+return 0;
+
+}
