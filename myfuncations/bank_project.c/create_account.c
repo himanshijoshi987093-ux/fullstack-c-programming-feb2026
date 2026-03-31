d374bb1b555b982e829beb122b62177cdf243fb1
himanshijoshi987093-ux <himanshijoshi987093@gmail.com>
2026-03-31 09:28:49 +0530

    this my changes

--- myfuncations/bank_project.c/create_account.c
+++ myfuncations/bank_project.c/create_account.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
+#include<string.h>
+void createaccount();
+void Deposit();
+void withdraw();
+void checkblance();
+void accountcreatedsuccessfully();
+void choiceno();
+int verifyNumberInstring(char stringdatap[11]);
+int totalblance;
+char MainAccount[11];
+
 int main()
-int createaccount()
-{
-
-}
-int withdrawamount()
-{
-
-}
 
 {
+
+    int choiceno;
+printf("#########Banking Management System ###########\n");
+
  printf("1. Create account:");
  Printf("2.Withdraw amount:");
  printf("3.Deposit Amount:");
@@ -17,8 +24,41 @@ int withdrawamount()
  peintf("5.Exit");
 
 
+do 
+{
 
+    printf("\n Please enter any choiceno number for account:\n");
+    scanf("%d",& choiceno);
 
+    switch (choiceno)
+{
+    case 1:
+    createaccount();
+    printf("\nplease enter the 11 digit number for create user account number:");
+    scanf("%d",&verifyNumberInstring);
+    break;
+    case 2:
+    Deposit();
+
+    break;
+    case 3:
+    withdraw();
+    break;
+
+    case 4:
+    checkblance();
+
+    break;
+    case 5:
+    accountcreatedsuccessfully();
+    break;
+
+    default:
+    printf(" \nError: please enter correct choise number");
+
+}
+
+}
 
 return 0;
 
